0fbc55d31cc265425f08f1bd4b468e6297a3dc6e
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2018-01-25 20:39:36 -0500

    tutorial 18 JSON

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 "use strict"
 
-var simbolo1 = Symbol();
-var simbolo2 = Symbol();
+var persona = { nombre: 'Mao' , twitter: 'ace'};
 
-var ambiente = Symbol('dev');
+var personas = [
+    {nombre: 'Mao0' , twitter: 'ace0'},
+    {nombre: 'Mao1' , twitter: 'ace1'},
+    {nombre: 'Mao2' , twitter: 'ace2'},
+    persona
+];
+
+//Convertir en texto
+var personaJSON = JSON.stringify(persona);
+
+var nuevaPersona = JSON.parse(personaJSON);
