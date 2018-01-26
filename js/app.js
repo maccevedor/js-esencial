a1a5a31721b3bac22a39642cf6d1126078a30ccb
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2018-01-26 01:03:00 -0500

    tutorial 11

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//Variables Globales
+var nombre = "Mao";
+
+
+console.log(nombre);
+
+function saludo(){
+    //variable locales
+    let nombre = "acevedo";
+    let edad = 20;
+    console.log(edad);
+}
+
+saludo();
