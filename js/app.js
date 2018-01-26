9e3b339bad82bb82622d34e1a327b18c0aab0de9
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2018-01-25 21:02:16 -0500

    tutorial 20

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
 "use strict"
 
-var persona = { nombre: 'Mao' , twitter: 'ace'};
-
-var personas = [
-    {nombre: 'Mao0' , twitter: 'ace0'},
-    {nombre: 'Mao1' , twitter: 'ace1'},
-    {nombre: 'Mao2' , twitter: 'ace2'},
-    persona
-];
-
-//Convertir en texto
-var personaJSON = JSON.stringify(persona);
-
-var nuevaPersona = JSON.parse(personaJSON);
