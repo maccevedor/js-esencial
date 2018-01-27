74314d4f477305d86b55d5fef81b95da146f2f21
maccevedor <maccevedor@gmail.com>
2018-01-27 16:34:29 -0500

    function anonima tutorial 41

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 "use strict"
  
-//parameter Spread
-function saludar(nombre,edad,ingredientes3,...otros){
-    var saldo = "Hola";
-    var resultado;
-    console.log(nombre + edad + ingredientes3 + ' otro '+ otros)
-    resultado = nombre + edad;
-    return resultado
+//Function Anonima
+
+// (
+//     function(){
+//         var mensaje = "Hola de nuevo";
+//         console.log(mensaje);
+//     }    
+// )()
+
+var saludar = function(nombre){
+        var mensaje = "Hola de nuevo " + nombre;
+        return mensaje;    
 }
-
-var ingredientesBase = ['Pollo','Tomate']
-
-saludar(...ingredientesBase,'Mao','pescado','chile'); 
