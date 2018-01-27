16f88b93218a2c0675e17b782056b5ee699954b3
maccevedor <maccevedor@gmail.com>
2018-01-27 16:52:57 -0500

    function arrow tutorial 43

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 "use strict"
  
-// callbaks
+//Function Arrow | Fat Arrow | Lambda
 
-function calcular(datoA,datoB,sumarCB,restarCB){
-    var sumar = datoA + datoB;
-    var restar = datoA - datoB;
+var saludar = nombre => "Hola" + nombre;
 
-    sumarCB(sumar);
-    restarCB(restar);
+console.log(saludar("Mao"));
+
+var sumar = cantidad=>cantidad+10;
+
+console.log(sumar(10));
+
+var calcular = (datoA,datoB) => datoA+datoB;
+console.log(calcular(15,10));
+
+var generar = (datoA,datoB) => {
+    var datoC = 5
+    return datoA+datoB+datoC;
 }
+console.log(generar(15,10));
 
-calcular (2,3,function(resultado){
-    console.log('suma ', resultado)
-},function(resultado){
-    console.log('Restar',resultado)
-})
+
+var validar = ()=>{
+    return "validacion correcta";
+}
+console.log(validar());
