d855b93c36d62d7d5a3b50497410a5e1e9e0cc1f
maccevedor <maccevedor@gmail.com>
2018-01-27 18:09:33 -0500

    Even tutorial 45

--- js/app.js
+++ js/app.js
@@ -3,11 +3,19 @@
 
 const boton = document.querySelector('.boton');
 
-// boton.addEventListener('click',function(){
-//     console.log(this.innerHTML);
-// }) 
+boton.addEventListener('click',function(){
+    console.log('pulsado');
+}) 
 
-boton.addEventListener('click',()=>{
-    console.log(this);
-    this.location = 'google.com';
-})
+boton.addEventListener('mouseover',function(){
+    console.log('sobre');
+}) 
+
+boton.addEventListener('mouseout',function(){
+    console.log('afuera');
+}) 
+
+// boton.addEventListener('click',()=>{
+//     console.log(this);
+//     this.location = 'google.com';
+// })
