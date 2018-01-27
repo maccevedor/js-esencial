a89f9c4ce1b8a48d6c20c0c1db0d8977f8b67040
maccevedor <maccevedor@gmail.com>
2018-01-27 15:55:35 -0500

    function tutorial 37

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 "use strict"
-
-var datoA = 10;
-var datoB = 20;
-var resultado = "sin datos";
-
-if( datoA > datoB ){
-    resultado = "la condicion"
+ 
+function saludar(nombre,edad){
+    var saldo = "Hola";
+    console.log(nombre + edad)
+    resultado = nombre + edad;
+    return resultado
 }
 
-conloso.log(resultado);
+saludar('Mao',29);
