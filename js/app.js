b6b9db0cf0aeb66c0f2adc32ed61e3cd3410ea9f
maccevedor <maccevedor@gmail.com>
2018-02-01 12:04:50 -0500

    Tutorial 58 search

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 "use strict"
 
+var mensaje = "Hola forest, Hola";
 
 
+var resultado;
+
+
+//resultado = mensaje.indexOf("Hola");
+resultado = mensaje.lastIndexOf("Hola");
+
+//incase sensative
+resultado  = mensaje.search(/ja/i)
+
+console.log(resultado);
