d0565451e1435236a945a3f510c0d90725e58520
maccevedor <maccevedor@gmail.com>
2018-02-01 15:57:12 -0500

    Tutorial 68 Array

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 "use strict"
 
-//comillas especiales
+//array
 
-var lenguaje = 'javascript';
+var platillos = ["ceviche","tacos","pastas"];
+var paises = ["peru","colombia"]
 
 
-var mensaje = `como  ${lenguaje}`;
+//var plato = platillos[0];
 
-var multilinea = `
-    hola mundo,
-    como se integra
-`;
+var menu = [platillos,paises];
 
-console.log(multilinea); 
+console.log(menu[0][0]);
+
+
+//push agrega elemento
+
+//pop saca un elemento
+
+//join retorna ocmo una cadena de texto 
+
+var platillosHTML = Array.from(document.querySelectorAll('.platillos p'));
+
+var platillos = platillosHTML.map(platillo => patillo.textContent)
+
+var platillos = Array.of("tacos","pastas")
+
+console.log(platillos)
