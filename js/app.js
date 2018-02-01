ccc6503e8021eef86b03248be4071b7b50f20e13
maccevedor <maccevedor@gmail.com>
2018-02-01 11:57:38 -0500

    Tutorial 57 medir var

--- js/app.js
+++ js/app.js
@@ -1,27 +1,4 @@
 "use strict"
-//document load
-//Timers
-
-
-window.addEventListener('load',function(){
-    
-});
-
-// var temporizador = setInterval(function (){
-//     setColor();
-// }, 2000);
-
-setTimeout(function(){
-    setColor();
-},3000)
 
 
 
-function setColor(){
-    var pagina = document.body
-    pagina.style.backgroundColor = pagina.style.backgroundColor == "red" ? "green" : "red"
-}
-
-function stopChangeColor(){
-    clearInterval(temporizador);
-}
