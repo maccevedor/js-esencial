bd4b31bc69a1ee88b1bbe78b84ea7bc985176c13
maccevedor <maccevedor@gmail.com>
2018-02-04 16:26:58 -0500

    Tutorial 89 Debugger

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
 "use strict"
 
-
-var valor1 = 100
-var valor2 = 10
-
-try{
-    if(valor1 < valor2){
-        console.log('pase');
-    }else{
-        throw new Error('Error por todo')
+class Producto{
+    constructor(numSerie){
+        this.numSerie = numSerie
+        this.tiempoGarantia = 100
     }
 
-}catch(error){
-    console.log(error.name);
+    static get infoTienda(){
+        console.log('productos');
+    }
+
+    set garantia(value){
+        this.tiempoGarantia -= value
+    }
+
+    get garantia(){
+        return this.tiempoGarantia
+    }
 }
+
+class Pantalla extends Producto{
+
+    constructor(numSerie,marca,modelo,pulgadas){
+        super(numSerie);
+        this.marca = marca;
+        this.modelo = modelo
+        this.pulgadas = pulgadas
+    }
+
+    encendido(){
+        this.garantia -1
+        console.log(`la pantalla ${this.marca} se enciende`)
+
+    }
+    volumen(){
+        console.log(`volumen `)
+    }
+    
+    info(){
+        console.log(` ${this.modelo} y  pulgadas ${this.pulgadas }`)
+    }
+
+    set peso(value){
+        this.kgs = value.trim();
+    }
+
+    get peso(){
+        return this.kgs
+    }
+
+}
+//instansias
+const tvSala = new Pantalla('111','Master','Oasis',50);
+const tvHabitacion = new Pantalla('222','Origin','Asus',80);
+
+
