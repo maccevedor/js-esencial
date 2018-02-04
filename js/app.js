1b9a3e600f5c9c054e70238f1d52e9f9e8438bc7
maccevedor <maccevedor@gmail.com>
2018-02-04 16:21:35 -0500

    Tutorial 88 Error

--- js/app.js
+++ js/app.js
@@ -1,57 +1,16 @@
 "use strict"
 
-class Producto{
-    constructor(numSerie){
-        this.numSerie = numSerie
-        this.tiempoGarantia = 100
+
+var valor1 = 100
+var valor2 = 10
+
+try{
+    if(valor1 < valor2){
+        console.log('pase');
+    }else{
+        throw new Error('Error por todo')
     }
 
-    static get infoTienda(){
-        console.log('productos');
-    }
-
-    set garantia(value){
-        this.tiempoGarantia -= value
-    }
-
-    get garantia(){
-        return this.tiempoGarantia
-    }
+}catch(error){
+    console.log(error.name);
 }
-
-class Pantalla extends Producto{
-
-    constructor(numSerie,marca,modelo,pulgadas){
-        super(numSerie);
-        this.marca = marca;
-        this.modelo = modelo
-        this.pulgadas = pulgadas
-    }
-
-    encendido(){
-        this.garantia -1
-        console.log(`la pantalla ${this.marca} se enciende`)
-
-    }
-    volumen(){
-        console.log(`volumen `)
-    }
-    
-    info(){
-        console.log(` ${this.modelo} y  pulgadas ${this.pulgadas }`)
-    }
-
-    set peso(value){
-        this.kgs = value.trim();
-    }
-
-    get peso(){
-        return this.kgs
-    }
-
-}
-//instansias
-const tvSala = new Pantalla('111','Master','Oasis',50);
-const tvHabitacion = new Pantalla('222','Origin','Asus',80);
-
-
