3987a3fd131aca72c139c73bfbdb95d69eade0de
maccevedor <maccevedor@gmail.com>
2018-02-06 18:23:53 -0500

    tutorial 5 Evaluar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 "use strict"
-console.log('hola');
+//console.log('hola');
 
-var expresion1 = new RegExp('abc');
+//var expresion1 = new RegExp('[0123456789]');
+
+var expresion1 = new RegExp('[0-9a-z]');
 
 var expresion2 = /abc/;
 
-console.log(expresion1.test('abced'));
+//console.log(expresion1.test('abced'));
+
+
+console.log(expresion1.test('1996'))
+
 
