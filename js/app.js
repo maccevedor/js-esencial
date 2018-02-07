64f0446d3fc3babc4e47d5bb53f00d6dd8267f40
maccevedor <maccevedor@gmail.com>
2018-02-07 09:59:38 -0500

    empezando el 18 terminado

--- js/app.js
+++ js/app.js
@@ -1,62 +1,21 @@
-"use strict"
-//console.log('hola');
+var exp1 = /y/g
 
-//var expresion1 = new RegExp('[0123456789]');
-
-var expresion1 = new RegExp('[0-9a-z]');
-
-var expresion2 = /abc/;
-
-//console.log(expresion1.test('abced'));
-
-//var fecha = /\d\d-\d\d-\d\d\d\d/
-
-//si cumple al menos un valor []
-//var exp = /[\d]/
-
-//negar un patron
-//var exp2 = /[^01]/
-//console.log(exp2.test('101002'))
-
-// sigbolo + , significa patron se va repetir 1 o mas
-// * significa 1 o mas veces o que no este presente
-//
+//var concidencia = exp1.exec('subr yfx sibr yfg')
 
 
-//var exp = /\d+/;
-//var exp = /\d*/;
+exp1.lastIndex = 6;
 
-//validar una fecha
-//{2,} minimo 2 maximo n
-//var exp = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
-//sin importar las mayusculas
-//var exp = /Hola mundo/i
-//agrupado de ext o sub expresiones
-//var exp = /Woo+(hoo+)+/i
+var concidencia = exp1.exec('subr yfx sibr yfg')
 
 
-//Woohoo!
-
-//limites de del valor
-//var exp = /\bcat\b/
-
-//opciones con el |
-//var exp = /pollo|res|pescado/
-
-//^ todo lo que hay al inicio , $ todo loq ue sucede al final
-//
-var exp = /^Any$/
-
-var expFin = /Javascrip$/
-
-var mensaje = 'sushi'
-
-//la primera aparicion
-console.log(mensaje.replace(/sushi/,'tacos'));
-//todas las que encuentre
-console.log(mensaje.replace(/sushi/g,'tacos'));
+console.log(concidencia);
+console.log(concidencia.lastIndex);
 
 
-console.log(exp.test('Any '));
-//console.log(exp);
-//console.log('hasta 1994'.match(/\d+/));
+var exp2 = /\d/g
+
+console.log(exp2.exec(' 4 tacos y agua'));
+console.log(exp2.exec(' 4 tacos y agua'));
+//esto para que empiece de neuvo
+exp2.lasIndex = 0
+console.log(exp2.exec(' 4 tacos y agua'));
