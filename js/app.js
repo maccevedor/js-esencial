2905edeac1e29382fddfb378b3a5d26514cc990a
maccevedor <maccevedor@gmail.com>
2018-02-08 10:53:39 -0500

    javascript avanzado 15

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-var persona = {}
+var persona = {},
+ estado = true,
+resultado = "";
+
 
 console.log(persona);
 
 
-if(persona){
+if(resultado){
 	console.log('p existe')
 }else{
 	console.log('p no existe')
