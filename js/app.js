892e7d451dda6df5eb1f13378761d1c2072f2f93
maccevedor <maccevedor@gmail.com>
2018-02-08 10:33:41 -0500

    javascript avanzado 13 for

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
-$(document).ready(function(){
-
-var tienda = {
-	nombre: 'tienda 4 esquinas',
-	calcular: function(costo1,costos2){
-		return costo1;	
-	},
+var persona = {
+	nombre: "Persona",
+	email: "mao@gmail.com",
+	twitter: 'asda',
 	saludar: function(){
-		var mensaje = "hola"
-		return mensaje;
+		return 'hola'
 	}
-};
+}
 
-console.log(tienda.nombre)
-console.log(tienda.calcular(20,30))
-console.log(tienda.saludar())
+var dato = ""
 
+for (dato in persona){
+	console.log(dato, persona[dato]);
 
-var btnSaludar = document.getElementById('btnSaludar');
-
-btnSaludar.addEventListener('click',function(){
-	console.log(tienda.saludar());
-
-});
-});
+}
