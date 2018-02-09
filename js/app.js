ed109581c5c243f6784cdb4a19536878b79524ba
maccevedor <maccevedor@gmail.com>
2018-02-08 19:45:37 -0500

    javascript avanzado 35 timeEnd

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-var animales = ['perro','gato']
+console.time('revision')
+
+var animales = ['p','a']
+
+for (var i = 0 ; i < 10000; i++){
+	animales[i] = i;
+}
+
+//terminar con la misma etiqueta
+console.timeEnd('revision')
 
 
-var listaAnimales = animales.join(',')
-var nuevaLista = animales.concat(',')
-console.log(listaAnimales);
-console.log(nuevaLista);
+
+
+
