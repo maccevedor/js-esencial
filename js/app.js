81f2507785a618c8a1faf487b374a0da8374ac6c
maccevedor <maccevedor@gmail.com>
2018-02-10 17:59:27 -0500

    Iniciando curso de ReactJS

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
-console.time('revision')
+//@ts-check
+console.time("revision");
 
-var animales = ['p','a']
+var animales = ["p", "a"];
 
-for (var i = 0 ; i < 10000; i++){
-	animales[i] = i;
+for (var i = 0; i < 10000; i++) {
+  animales[i] = i;
 }
 
 //terminar con la misma etiqueta
-console.timeEnd('revision')
-
-
-
-
-
+console.timeEnd("revision");
